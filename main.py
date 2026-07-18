c9d28a3872c4b2a224dc545d85caf933977fa27e
XorazmAl <yusupov7075@gmail.com>
2026-07-18 10:30:16 +0500

    Refactor bot server startup process
    Refactor the keep_alive function to start the app directly in the main block.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-i
-   import os
+import os
 import telebot
 from flask import Flask
 from threading import Thread
 
-# Tokeningizni mana bu yerga, tirnoq ichiga yozing
+# Tokeningizni mana bu yerga joylang
 API_TOKEN = '8860187470:AAGge6qQ95Fa9GzmEt-BgudJ4-d'
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -14,17 +13,14 @@ app = Flask(__name__)
 def home():
     return "Bot ishlayapti!"
 
-def run():
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Salom! Men ishlayapman.")
 
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
 if __name__ == "__main__":
-    keep_alive()
+    Thread(target=run).start()
     bot.infinity_polling()
+
