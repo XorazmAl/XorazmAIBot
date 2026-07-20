be4a1498a17ade81e211ac2579ea95d09223530b
XorazmAl <yusupov7075@gmail.com>
2026-07-20 13:05:40 +0500

    Change API token in main.py
    Updated API token for the Telegram bot.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import telebot
 from flask import Flask
 from threading import Thread
-API_TOKEN = "8860187470:AAFQj-YRPF2dv03rzrviVvkwEkOo5seJkLsos"
+API_TOKEN = "8860187470:AAGzdkoyPt6DBj5HRj4Y77E8DShZtX_i5RM"
 bot = telebot.TeleBot(API_TOKEN)
 app = Flask(__name__)
 
